a6bd2d7e2221ae3b280f6edaf4bc6396eb85e407
nhlam3011 <135177207+nhlam3011@users.noreply.github.com>
2023-06-13 15:40:31 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function addHeSoInput() {
     var bac = parseInt(document.getElementById("bac").value);
     var heSoContainer = document.getElementById("heSoContainer");
     heSoContainer.innerHTML = "";
-
     for (var i = 0; i <= bac; i++) {
         var label = document.createElement("label");
         label.innerHTML = "Hệ số x" + i + ": ";
@@ -32,10 +31,8 @@ function addMatrixInputs() {
     var size = parseInt(document.getElementById("size").value);
     var matrixContainer = document.getElementById("matrixContainer");
     matrixContainer.innerHTML = "";
-
     for (var i = 0; i < size; i++) {
         var row = document.createElement("div");
-
         for (var j = 0; j < size + 1; j++) {
             var input = document.createElement("input");
             input.type = "number";
@@ -43,7 +40,6 @@ function addMatrixInputs() {
             input.id = "matrix" + i + "-" + j;
             row.appendChild(input);
         }
-
         matrixContainer.appendChild(row);
     }
 }
@@ -51,29 +47,22 @@ function addMatrixInputs() {
 function solveChiaDoi() {
     var bac = parseInt(document.getElementById("bac").value);
     var heSo = [];
-
     for (var i = 0; i <= bac; i++) {
         heSo[i] = parseFloat(document.getElementById("x" + i).value);
     }
-
     var a = parseFloat(document.getElementById("a").value);
     var b = parseFloat(document.getElementById("b").value);
     var epsilon = parseFloat(document.getElementById("epsilon").value);
-
     var fA = calculateFunctionValue(a, heSo);
     var fB = calculateFunctionValue(b, heSo);
-
     if (fA * fB >= 0) {
         displaySolution("no-solution");
         return;
     }
-
     var x;
-
     while (Math.abs(b - a) > epsilon) {
         x = (a + b) / 2;
         var fX = calculateFunctionValue(x, heSo);
-
         if (fX === 0) {
             break;
         } else if (fX * fA < 0) {
@@ -83,24 +72,20 @@ function solveChiaDoi() {
             fA = fX;
         }
     }
-
     displaySolution(x.toFixed(5));
 }
 
 function calculateFunctionValue(x, coefficients) {
     var result = 0;
-
     for (var i = 0; i < coefficients.length; i++) {
         result += coefficients[i] * Math.pow(x, i);
     }
-
     return result;
 }
 
 function solveGauss() {
     var size = parseInt(document.getElementById("size").value);
     var matrix = [];
-
     for (var i = 0; i < size; i++) {
         matrix[i] = [];
         for (var j = 0; j <= size; j++) {
@@ -108,7 +93,6 @@ function solveGauss() {
             matrix[i][j] = parseFloat(document.getElementById(inputId).value);
         }
     }
-
     for (var i = 0; i < size; i++) {
         var maxRow = i;
         for (var j = i + 1; j < size; j++) {
@@ -116,13 +100,11 @@ function solveGauss() {
                 maxRow = j;
             }
         }
-
         for (var k = i; k <= size; k++) {
             var temp = matrix[maxRow][k];
             matrix[maxRow][k] = matrix[i][k];
             matrix[i][k] = temp;
         }
-
         for (var j = i + 1; j < size; j++) {
             var factor = matrix[j][i] / matrix[i][i];
             for (var k = i; k <= size; k++) {
@@ -130,11 +112,9 @@ function solveGauss() {
             }
         }
     }
-
     var solution = [];
     var isNoSolution = false;
     var isInfiniteSolutions = false;
-
     for (var i = size - 1; i >= 0; i--) {
         if (matrix[i][i] === 0 && matrix[i][size] !== 0) {
             isNoSolution = true;
@@ -143,20 +123,18 @@ function solveGauss() {
             isInfiniteSolutions = true;
             break;
         }
-
         solution[i] = matrix[i][size];
         for (var j = i + 1; j < size; j++) {
             solution[i] -= matrix[i][j] * solution[j];
         }
         solution[i] /= matrix[i][i];
     }
-
     displaySolution(solution, isNoSolution, isInfiniteSolutions);
 }
 
 function displaySolution(solution, isNoSolution, isInfiniteSolutions) {
     var solutionContainer = document.getElementById("solution");
-
+    solutionContainer.innerHTML = "<p>" + "Nghiệm x= " + solution + "</p>";
     if (isNoSolution) {
         solutionContainer.innerHTML = "<p>Hệ phương trình vô nghiệm</p>";
     } else if (isInfiniteSolutions) {
@@ -171,7 +149,6 @@ function displaySolution(solution, isNoSolution, isInfiniteSolutions) {
         tableHeader.appendChild(variableHeader);
         tableHeader.appendChild(valueHeader);
         table.appendChild(tableHeader);
-
         for (var i = 0; i < solution.length; i++) {
             var row = document.createElement("tr");
             var variableCell = document.createElement("td");
@@ -182,7 +159,6 @@ function displaySolution(solution, isNoSolution, isInfiniteSolutions) {
             row.appendChild(valueCell);
             table.appendChild(row);
         }
-
         solutionContainer.innerHTML = "";
         solutionContainer.appendChild(table);
     }
