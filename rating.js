754772eb27a053772db0729eefd90a2084303f19
nhlam3011 <135177207+nhlam3011@users.noreply.github.com>
2023-06-13 17:17:50 +0700

    Update rating.js

--- rating.js
+++ rating.js
@@ -1,3 +1,94 @@
+// Function to save data to Local Storage
+function saveDataToLocalStorage(fullName, task, rating) {
+    // Check if Local Storage is supported
+    if (typeof(Storage) !== "undefined") {
+        // Get existing data from Local Storage or create an empty array
+        let data = JSON.parse(localStorage.getItem("ratings")) || [];
+
+        // Create a new rating object
+        const newRating = {
+            fullName: fullName,
+            task: task,
+            rating: rating
+        };
+
+        // Add the new rating object to the data array
+        data.push(newRating);
+
+        // Save the updated data array to Local Storage
+        localStorage.setItem("ratings", JSON.stringify(data));
+    } else {
+        console.log("Local Storage is not supported");
+    }
+}
+
+// Function to load data from Local Storage
+function loadRatingsFromLocalStorage() {
+    // Check if Local Storage is supported
+    if (typeof(Storage) !== "undefined") {
+        // Get the saved data from Local Storage
+        const data = JSON.parse(localStorage.getItem("ratings"));
+
+        // Check if there is any saved data
+        if (data) {
+            // Loop through the data and add rows to the rating table
+            data.forEach(function(rating) {
+                const newRow = document.createElement('tr');
+                newRow.innerHTML = `
+                    <td>${rating.fullName}</td>
+                    <td>${rating.task}</td>
+                    <td>${rating.rating}</td>
+                    <td>
+                        <button class="edit-btn">Edit</button>
+                        <button class="delete-btn">Delete</button>
+                    </td>
+                `;
+
+                // Add class to the rating column based on the selected rating
+                const ratingColumn = newRow.querySelector('td:nth-child(3)');
+                ratingColumn.classList.add(`rating-${rating.rating}`);
+
+                ratingTableBody.appendChild(newRow);
+            });
+        }
+    } else {
+        console.log("Local Storage is not supported");
+    }
+}
+
+// Call the loadRatingsFromLocalStorage function when the page loads
+window.addEventListener('load', loadRatingsFromLocalStorage);
+
+// Event listener for the rating form submission
+ratingForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const fullName = document.getElementById('full_name').value;
+    const task = document.getElementById('task').value;
+    const rating = document.querySelector('input[name="rating"]:checked').value;
+
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td>${fullName}</td>
+        <td>${task}</td>
+        <td>${rating}</td>
+        <td>
+            <button class="edit-btn">Edit</button>
+            <button class="delete-btn">Delete</button>
+        </td>
+    `;
+
+    // Add class to the rating column based on the selected rating
+    const ratingColumn = newRow.querySelector('td:nth-child(3)');
+    ratingColumn.classList.add(`rating-${rating}`);
+
+    ratingTableBody.appendChild(newRow);
+    ratingForm.reset();
+
+    // Save the data to Local Storage
+    saveDataToLocalStorage(fullName, task, rating);
+});
+
 const ratingForm = document.getElementById('ratingForm');
 const ratingTableBody = document.getElementById('ratingTableBody');
 
